3426d34a281be729e34f41770585bd9192747058
Siddhant Gosavi <sgosavi@conga.com>
2022-01-05 16:17:40 +0530

    added terraform backend

--- terraform/basic_stack/main.tf
+++ terraform/basic_stack/main.tf
@@ -1,7 +1,12 @@
 
 # Configure the Alicloud Provider
+
+# Run the following commands before running init
+
+# export ALICLOUD_ACCESS_KEY="anaccesskey"
+# export ALICLOUD_SECRET_KEY="asecretkey"
+# export ALICLOUD_REGION="cn-beijing"
+
 provider "alicloud" {
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
-  region     = "${var.region}"
+    
 }
